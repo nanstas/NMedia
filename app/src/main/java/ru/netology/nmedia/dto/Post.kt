cd86d633d7ac1328b71023155f566eab47f4b9f9
Stanislav Nanoshkin <nanstas@yandex.ru>
2020-10-19 19:07:21 +0500

    add like and share logic

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likedByMe: Boolean = false,
+    var countLikes: Int = 0,
+    var countShares: Int = 0
+)
