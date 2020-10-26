aa08b09dc6912c2f9cdc4c3eae5f7f7826028b27
Stanislav Nanoshkin <nanstas@yandex.ru>
2020-10-26 16:00:19 +0500

    implement RecyclerView

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -14,32 +15,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        viewModel.data.observe(this, { post ->
-            with(binding) {
-                authorTextView.text = post.author
-                publishedTextView.text = post.published
-                contentTextView.text = post.content
-                likeImageView.setImageResource(
-                    if (post.likedByMe) R.drawable.ic_baseline_like_active_24 else R.drawable.ic_baseline_like_border_24
-                )
-                likeCountsTextView.text = numToPostfix(post.countLikes)
-                shareCountsTextView.text = numToPostfix(post.countShares)
-            }
-        })
+        val adapter = PostsAdapter(
+            {viewModel.likeById(it.id)},
+            {viewModel.shareById(it.id)}
+        )
 
-        binding.likeImageView.setOnClickListener {
-            viewModel.like()
-        }
-
-        binding.shareImageView.setOnClickListener {
-            viewModel.share()
+        binding.listRecyclerView.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.list = posts
         }
     }
-}
-
-fun numToPostfix(num: Int): String = when (num) {
-    in 0..999 -> num.toString()
-    in 1_000..9_999 ->  if (num.toString()[1] == '0') "${num.toString()[0]}K" else "${num.toString()[0]},${num.toString()[1]}K"
-    in 10_000..999_999 -> "${num.toString().dropLast(3)}K"
-    else -> if (num.toString()[1] == '0') "${num.toString()[0]}M" else "${num.toString()[0]},${num.toString()[1]}M"
 }
