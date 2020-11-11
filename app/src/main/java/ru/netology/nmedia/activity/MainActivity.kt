1bd02b9ce2098008b795dca72eb84dfea56927b5
Stanislav Nanoshkin <nanstas@yandex.ru>
2020-11-11 12:55:48 +0500

    add Post in own Activity

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.netology.nmedia.activity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
@@ -16,16 +18,20 @@ import ru.netology.nmedia.utils.Utils
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val newPostRequestCode = 1
+    val viewModel: PostViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewModel: PostViewModel by viewModels()
         val adapter = PostsAdapter(object : OnInteractionListener {
             override fun onEdit(post: Post) {
                 viewModel.edit(post)
-                groupCancel.visibility = View.VISIBLE
+//                groupCancel.visibility = View.VISIBLE
+                val intent = Intent(this@MainActivity, NewPostActivity::class.java)
+                startActivityForResult(intent, newPostRequestCode)
             }
 
             override fun onRemove(post: Post) {
@@ -50,38 +56,58 @@ class MainActivity : AppCompatActivity() {
             if (post.id == 0L) {
                 return@observe
             }
-            with(binding.contentEditText) {
-                requestFocus()
-                setText(post.content)
-            }
+//            with(binding.contentEditText) {
+//                requestFocus()
+//                setText(post.content)
+//            }
         })
 
-        binding.saveImageButton.setOnClickListener {
-            with(binding.contentEditText) {
-                if (TextUtils.isEmpty(text)) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        context.getString(R.string.error_empty_content),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-
-                viewModel.changeContent(text.toString())
-                viewModel.save()
-                groupCancel.visibility = View.INVISIBLE
-                setText("")
-                clearFocus()
-                Utils.hideKeyboard(this)
-            }
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewPostActivity::class.java)
+            startActivityForResult(intent, newPostRequestCode)
         }
 
-        binding.cancelImageButton.setOnClickListener {
-            with(binding.contentEditText) {
-                groupCancel.visibility = View.INVISIBLE
-                setText("")
-                clearFocus()
-                Utils.hideKeyboard(this)
+//        binding.saveImageButton.setOnClickListener {
+//            with(binding.contentEditText) {
+//                if (TextUtils.isEmpty(text)) {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        context.getString(R.string.error_empty_content),
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                    return@setOnClickListener
+//                }
+//
+//                viewModel.changeContent(text.toString())
+//                viewModel.save()
+//                groupCancel.visibility = View.INVISIBLE
+//                setText("")
+//                clearFocus()
+//                Utils.hideKeyboard(this)
+//            }
+//        }
+//
+//        binding.cancelImageButton.setOnClickListener {
+//            with(binding.contentEditText) {
+//                groupCancel.visibility = View.INVISIBLE
+//                setText("")
+//                clearFocus()
+//                Utils.hideKeyboard(this)
+//            }
+//        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            newPostRequestCode -> {
+                if (resultCode != Activity.RESULT_OK) {
+                    return
+                }
+
+                data?.getStringExtra(Intent.EXTRA_TEXT)?.let {
+                    viewModel.changeContent(it)
+                    viewModel.save()
+                }
             }
         }
     }
